e0fa813ea76ec9f1fff6ea683c8e0df45d676f83
Cameron Diver <cameron.diver94@gmail.com>
2016-01-01 18:20:42 +0000

    Added divide function to pert

--- src/perft.hs
+++ src/perft.hs
@@ -1,6 +1,6 @@
 module Perft where
 
-import Data.Sequence
+import Data.Sequence as S
 
 import ChessData
 import Moves
@@ -14,9 +14,14 @@ perft :: Depth -> GameState -> [(Depth, Int)]
 perft d g = perft' 1 d g
   where
     perft' :: Depth -> Depth -> GameState -> [(Depth, Int)]
-    perft' c d g = if c /= d
-                   then (c, Data.Sequence.length $ movesAtDepth c g) : perft' (c+1) d g
-                   else [(c, Data.Sequence.length $ movesAtDepth c g)]
+    perft' c d g
+      | c == d = [(c, S.length $ movesAtDepth c g)]
+      | d == 0 || c == 0 = [(0, 1)]
+      | otherwise = (c, S.length $ movesAtDepth c g) : perft' (c+1) d g
+
+-- Run a single depth of divide
+divide :: Depth -> Seq GameState -> Seq (Maybe Move, Int)
+divide d gs = fmap (\x -> (move x, snd $ last $ perft (d-1) x)) gs
 
 movesAtDepth :: Depth -> GameState -> Seq GameState
 movesAtDepth 1 g = genMoves g
